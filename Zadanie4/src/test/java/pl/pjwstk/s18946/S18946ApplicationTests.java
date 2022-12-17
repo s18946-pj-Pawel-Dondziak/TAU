340c49c614469762a58f1a5b1ef6dd4674a9e440
s18946-pj-Pawel-Dondziak <s18946@pjwstk.edu.pl>
2022-12-17 16:52:55 +0100

    zadanie5

---
+++ Zadanie4/src/test/java/pl/pjwstk/s18946/S18946ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.s18946;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18946ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
